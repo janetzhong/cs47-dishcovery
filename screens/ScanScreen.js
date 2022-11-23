b17ff1e99aff9a2d770eb8a6359f3cfe1115afa7
Kyla Guru <kylaguru@KylasNeMacBook2.hsd1.il.comcast.net>
2022-11-22 20:45:59 -0600

    logic for filtering prediction results

--- screens/ScanScreen.js
+++ screens/ScanScreen.js
@@ -42,21 +42,22 @@ const Clarifai = require('clarifai');
 import ingredientContext from '../assets/ingredientContext';
 
 const ingredientContexttest = ingredientContext
-console.log(Object.keys(ingredientContexttest))
+// console.log(Object.keys(ingredientContexttest))
 
-const predictions = ["pred1", "pred2"]
+// const predictions = ["pred1", "pred2"]
 
-console.log(Object.keys(ingredientContexttest).includes("bitter melon"))
+// console.log(Object.keys(ingredientContexttest).includes("bitter melon"))
 
-const arr1 = Object.keys(ingredientContexttest);
-const arr2 = predictions;
+// const ingredientList = Object.keys(ingredientContexttest);
+// const preds = predictions;
 
-const contains = arr1.some(element => {
-  if arr2.includes(element) {
-    return element;
-  }
-});
-console.log(contains); // 👉️ true
+// const foundItem = ingredientList.some(element => {
+//   if (preds.includes(element)) { 
+//     // return name of ingredient found in prediction results
+//     return element;
+//   }
+// });
+// console.log(foundItem); 
 
 
 const clarifai = new Clarifai.App({
@@ -87,6 +88,7 @@ export default class App extends React.Component {
       );
       return manipulatedImage.base64;
     };
+
     predict = async image => {
       let predictions = await clarifai.models.predict(
         Clarifai.FOOD_MODEL,
@@ -94,16 +96,27 @@ export default class App extends React.Component {
       );
       return predictions;
     };
-
     objectDetection = async () => {
       let photo = await this.capturePhoto();
       let resized = await this.resize(photo);
       let predictions = await this.predict(resized);
       this.setState({ predictions: predictions.outputs[0].data.concepts });
     };
-  
+    
+    identifyIngredient = async predictions => {
+      const ingredientList = Object.keys(ingredientContexttest);
+      const foundItem = ingredientList.some(element => {
+        if (preds.includes(element)) { 
+          // return name of ingredient found in prediction results
+          return element;
+        }
+      });
+      return foundItem; // string representing identified ingredient
+  };
     render() {
+      // make the button change state and be actionable - ingredient detected and then be clickable for context
       const { hasCameraPermission, predictions } = this.state;
+      const filteredPredictions = predictions.filter(({key}) => Object.keys(ingredientContexttest).includes(key));
       if (hasCameraPermission === null) {
         return <View />;
       } else if (hasCameraPermission === false) {
@@ -134,7 +147,11 @@ export default class App extends React.Component {
                   }}
                 >
                   <FlatList
-                    data={predictions.map(prediction => ({
+                  // filteredpredictions: from predictions dictionary, 
+                  // keep only the ones where the key (prediction)
+                  // is in ingredientContextList.keys()
+                  // filteredpredictions is a dictionary
+                    data={filteredPredictions.map(prediction => ({
                       key: `${prediction.name} ${prediction.value}`,
                     }))}
                     renderItem={({ item }) => (
