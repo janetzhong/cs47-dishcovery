cf59121155f379ba3c64a3ee6f5519c716020080
Amrita Palaparthi <amritapv@stanford.edu>
2022-12-07 20:33:43 -0800

    combining with kyla add context changes

--- assets/ingredientContext.js
+++ assets/ingredientContext.js
@@ -1,11 +1,13 @@
 export default {
-    "bitter melon": {
+    "bittermelon": {
         "flavor profile/texture": "• Sharp, bitter flavour that is toned down through cooking\n• Crunchy husk, similar to that of bell pepper\n• Crunchy, watery inside similar to cucumber",
-        "health benefits": "superfood",
-        "cultural context": "some other blurb",
-        "origins and geography": "other bittery blurb",
+        "health benefits": "Bittermelon is rich in antioxidants and supports healthy digestion. One cup of bittermelon contains about 43% of your recommended daily intake of Vitamin C.",
+        "cultural context": "• Chinese cooking: stir fried with pork, used in soups and consumed as a tea.\n• Northern Indian Bitter Melon: stuffed with spices, prepared or served with yogurt to offset the bitterness.\n• Southern India: mixed with coconut and stir-fried with various spices.\n•Caribbean: Bitter Melon is sautéed with onion, garlic until crisp.\n•Haiti: herbal tea (asosi)",
+        "new cultural context": {"• Chinese cooking": "stir fried with pork, used in soups and consumed as a tea.", "• Northern Indian Bitter Melon": "stuffed with spices, prepared or served with yogurt to offset the bitterness.", "• Southern India": "mixed with coconut and stir-fried with various spices.", "• Caribbean": "Bitter Melon is sautéed with onion, garlic until crisp.", "• Haiti": "herbal tea (asosi)"},
+        "origins and geography": "Bittermelon has a mixed origin, likely originating in Africa. Wild variants were spread in Asia and South-East Asia and it is also found in the Carribean.",
         "image": "bittermelon",
         "alternate names": "bitter gourd\nkarela",
+        "new alternate names": {"English": "Bitter Gourd              ", "Hindi": "Karela              ", "Malay": "Peria              ", "Tagalog": "Ampalaya              ", "Chinese": "kǔguā              ", "Haitian": "Asosi              "}
     },
     "lemongrass": {
         "flavor profile/texture": "lemony",
--- screens/AdditionalContextScreen.js
+++ screens/AdditionalContextScreen.js
@@ -12,8 +12,10 @@ import styles from '../assets/styles/ContextFlow.style';
 import bittermelon from '../assets/images/bittermelon.jpg';
 import cardamom from '../assets/images/cardamom.png';
 import lemongrass from '../assets/images/lemongrass.png';
+import bittermelon_origins from "../assets/images/bittermelon_origins.png";
 import commonStyles from '../assets/styles/CommonStyles.styles';
 import textStyles from '../assets/styles/TextStyles.style';
+import { COLORS } from '../constants';
 
 const ingredientContexttest = ingredientContext
 
@@ -39,7 +41,7 @@ export default class App extends React.Component {
     const itemKeySplit = itemKey.split(" Accuracy");     // ['bitter melon, ' Accuracy 0.99...'] This is not a good way to do it because it requires Accuracy be in that string but i was confused how to send filteredPredictions to params and so i sent item.key instead
     const itemName = itemKeySplit.shift(); 
     const capItemName = this.capitalizeFirstLetters(itemName);
-    //this.useEffect(capItemName)
+    this.useEffect(capItemName)
     switch(ingredientContexttest[itemName]["image"]) {
       case 'bittermelon':
         var imageName = bittermelon
@@ -70,9 +72,9 @@ export default class App extends React.Component {
             name: '',
             customInnerItem: (
               <View style={styles.innerExpandBox}>
-                <View style={styles.expandImageBox}>
+                {/* <View style={styles.expandImageBox}>
                   <Image source={imageName} style={styles.expandImage}></Image>
-                </View>
+                </View> */}
                 <View style={styles.textContainerStyle}>
                   <Text style={styles.TextStyle}>{ingredientContexttest[itemName]["flavor profile/texture"]}</Text>
                 </View>
@@ -90,11 +92,16 @@ export default class App extends React.Component {
             name: '',
             customInnerItem: (
               <View style={styles.innerExpandBox}>
-                <View style={styles.expandImageBox}>
+                {/* <View style={styles.expandImageBox}>
                   <Image source={innerImageName} style={styles.expandImage}></Image>
-                </View>
+                </View> */}
                 <View style={styles.textContainerStyle}>
-                  <Text style={styles.TextStyle}>{ingredientContexttest[itemName]["alternate names"]}</Text>
+                  {Object.entries(ingredientContexttest[itemName]["new alternate names"]).map(([key, value]) => (
+                      <Text style={{fontSize: 15, lineHeight: 25}}>
+                      <Text style={{fontWeight: 'bold', color: COLORS.dishcoveryOrange}}>{key}: </Text>
+                      <Text style={{fontWeight: 'normal'}}>{value}</Text>
+                      </Text>
+                  ))}
                 </View>
               </View> 
             ),
@@ -110,11 +117,13 @@ export default class App extends React.Component {
             name: '',
             customInnerItem: (
               <View style={styles.innerExpandBox}>
-                <View style={styles.expandImageBox}>
-                  <Image source={imageName} style={styles.expandImage}></Image>
-                </View>
                 <View style={styles.textContainerStyle}>
-                  <Text style={styles.TextStyle}>{ingredientContexttest[itemName]["cultural context"]}</Text>
+                  {Object.entries(ingredientContexttest[itemName]["new cultural context"]).map(([key, value]) => (
+                      <Text style={{fontSize: 15, lineHeight: 25}}>
+                      <Text style={{fontWeight: 'bold', color: COLORS.dishcoveryOrange}}>{key}: </Text>
+                      <Text style={{fontWeight: 'normal'}}>{value}</Text>
+                      </Text>
+                  ))}
                 </View>
               </View> 
             ),
@@ -131,7 +140,7 @@ export default class App extends React.Component {
             customInnerItem: (
               <View style={styles.innerExpandBox}>
                 <View style={styles.expandImageBox}>
-                  <Image source={imageName} style={styles.expandImage}></Image>
+                  <Image source={bittermelon_origins} style={styles.expandImage}></Image>
                 </View>
                 <View style={styles.textContainerStyle}>
                   <Text style={styles.TextStyle}>{ingredientContexttest[itemName]["origins and geography"]}</Text>
@@ -150,9 +159,9 @@ export default class App extends React.Component {
             name: '',
             customInnerItem: (
               <View style={styles.innerExpandBox}>
-                <View style={styles.expandImageBox}>
+                {/* <View style={styles.expandImageBox}>
                   <Image source={imageName} style={styles.expandImage}></Image>
-                </View>
+                </View> */}
                 <View style={styles.textContainerStyle}>
                   <Text style={styles.TextStyle}>{ingredientContexttest[itemName]["health benefits"]}</Text>
                 </View>
