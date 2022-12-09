7629afea48fe5f6283b4745babcd77b6bcf3c53b
Amrita Palaparthi <amritapv@stanford.edu>
2022-12-08 19:05:52 -0800

    removing rando button in profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -71,9 +71,6 @@ export default class App extends React.Component {
                             
                         </View>
                         <View style={styles.addMoreView}><Text style={textStyles.linkText}>Add More ›</Text></View>
-                        <TouchableOpacity style={styles.buttonContainer} activeOpacity = { .5 } onPress={ () => navigation.navigate("Scan Screen")}>
-                        <Text style={styles.ButtonTextStyle}> Begin Scan </Text>
-                        </TouchableOpacity>
                     </View>
                 </View>
             </SafeAreaView>
